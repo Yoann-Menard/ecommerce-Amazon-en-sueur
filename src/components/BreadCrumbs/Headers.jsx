e2508d4416abe49c667c4879776e43e617bebeb9
Yoann <yoann.menard@epitech.eu>
2022-06-03 16:40:44 +0200

    [master] - Removed unused importation

--- src/components/BreadCrumbs/Headers.jsx
+++ src/components/BreadCrumbs/Headers.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import  AppBreadcrumbs  from "./BreadCrumbs";
-// import { Row, Col } from 'react-bootstrap';
 
 export default class Header extends Component {
     render(){
